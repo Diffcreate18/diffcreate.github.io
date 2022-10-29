bf93ea249654c55c80f6ea688bd4d080e969edf0
diffcreate <112700046+diffcreate@users.noreply.github.com>
2022-10-30 01:48:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ document.getElementById("offcanvasDarkNavbar").className="offcanvas offcanvas-en
 document.getElementById("progress").style.background = "black";
 document.getElementById("progress-bar").style.color = "white";
 document.getElementById("welcome").style.color = "black";
+document.getElementById("anchor").src="images/eyeball_close.png";
+document.getElementById("eyes").style.display = "none";
 }
 
 function DarkMode()
@@ -21,6 +23,7 @@ function DarkMode()
     a++;
     if(a == 10){
         document.getElementById("bg").style.background = "red";
+        
     }else if(a == 20){
         a = 0;
     }else{
@@ -32,6 +35,8 @@ document.getElementById("offcanvasDarkNavbar").className="offcanvas offcanvas-en
 document.getElementById("progress").style.background = "white";
 document.getElementById("progress-bar").style.color = "black";
 document.getElementById("welcome").style.color = "white";
+document.getElementById("anchor").src="images/eyeball.png";
+document.getElementById("eyes").style.display = "block";
 }
 
 var x = document.getElementById("progress-bar");
@@ -42,3 +47,29 @@ function end(){
  document.getElementById("welcome").style.display = "block";
 }
 
+document.addEventListener('mousemove', (e) => {
+    console.log(e)
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const anchor = document.getElementById('anchor')
+    const rekt = anchor.getBoundingClientRect();
+    const anchorX = rekt.right + rekt.width / 2;
+    const anchorY = rekt.top + rekt.height / 2;
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+    const eyes = document.querySelectorAll('.eye')
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+    })
+})
+
+function angle(cx, cy, ex, ey){
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = rad * 180 / Math.PI;
+    return deg;
+}
